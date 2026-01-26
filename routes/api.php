d80d12915e68d71d5f0e7b6ff1c9ad0bfca13439
samirjisan <samir1244@gmail.com>
2026-01-26 16:57:09 +0600

    new branch

--- routes/api.php
+++ routes/api.php
@@ -72,6 +72,7 @@ Route::prefix('activity-types')->group(function () {
 
 Route::prefix('kam-performance')->group(function () {
     Route::get('/', [KamPerformanceController::class, 'index']);
+    Route::get('/kam-performance-breakdown', [KamPerformanceController::class, 'getTransferredPreviousMonthBreakdown']);
 });
 
 
